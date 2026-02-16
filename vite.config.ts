9a143fa87494bfaccbfdcb2d1bd8b0d951e9400f
Fiska Yeh <fiskayeh@Fiskas-Mac-mini.local>
2026-02-17 02:44:55 +0800

    modify base in vite.config.tx

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 使用相對路徑，確保部署在 GitHub Pages 子路徑時資源讀取正確
+  base: '/ohaeven_media/', // 使用相對路徑，確保部署在 GitHub Pages 子路徑時資源讀取正確
   server: {
     port: 5173,
     open: true
